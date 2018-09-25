4f7ec88fbbfb5a7efa93728b554b7977329baa39
yumcoder-plusplus <yumcoder.platform@gmail.com>
2018-09-25 13:59:13 +0330

    bug fixed

---
+++ cmd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#####################################################
+if [ $1 = "clear" ]
+then
+    docker rm $(docker ps -aq -f name=yumd)
+    docker rm $(docker network ls -q -f name=dev_net)
+fi
+#####################################################
+if [ $1 = "up" ]
+then
+    docker-compose up -d
+fi
+#####################################################
+if [ $1 = "down" ]
+then
+    docker-compose down
+fi
